43548d631b4960900be0207bd466077bf33f919a
Dmitriy Nikiforov <nikiforov_d.1973@mail.ru>
2023-05-02 22:01:49 +0400

    add serializer

--- backendshop/backendshop/settings.py
+++ backendshop/backendshop/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backendshop'
-    'django_rest_passwordreset'
+    'backendshop',
+
 ]
 
 MIDDLEWARE = [
--- backendshop/shops/models.py
+++ backendshop/shops/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from backendshop.auth_user.models import User, Contact
+
 STATUS_CHOICES = (
     ('Статус корзины'),
     ('Новый'),
@@ -9,6 +11,8 @@ STATUS_CHOICES = (
     ('Доставлен'),
     ('Отменен'),
 )
+
+
 class Shop(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название магазина')
     url = models.URLField(verbose_name='Сайт магазина', null=True, blank=True)
@@ -57,10 +61,11 @@ class InfoProduct(models.Model):
     quantity = models.PositiveIntegerField(verbose_name='Количество')
     price = models.PositiveIntegerField(verbose_name='Цена')
     suggested_retail_price = models.PositiveIntegerField(verbose_name='Рекомендуемая розничная цена')
-    product = models.ForeignKey(Product,verbose_name='Продукт', related_name='product_infos',
+    product = models.ForeignKey(Product, verbose_name='Продукт', related_name='product_infos',
                                 blank=True, on_delete=models.CASCADE)
-    shop = models.ForeignKey(Shop, verbose_name='Магазин',related_name='product_infos',
+    shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='product_infos',
                              blank=True, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = 'Информация о продукте'
         verbose_name_plural = 'Информационный список о продуктах'
@@ -70,7 +75,6 @@ class InfoProduct(models.Model):
 
     def __str__(self):
         return f'{self.shop.name} - {self.product.name}'
-
 
 
 class Parameter(models.Model):
@@ -83,6 +87,7 @@ class Parameter(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class ProductParameter(models.Model):
     info_product = models.ForeignKey(InfoProduct, verbose_name='Информация о продукте', blank=True,
@@ -100,6 +105,7 @@ class ProductParameter(models.Model):
     def __str__(self):
         return f'{self.info_product.model} - {self.parameter.name}'
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, verbose_name='Пользователь', related_name='orders', blank=True,
                              on_delete=models.CASCADE)
@@ -116,10 +122,11 @@ class Order(models.Model):
     def __str__(self):
         return f'{self.user} - {self.data_time}'
 
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, verbose_name='Заказ', related_name='ordered_items',
-                              blank=True,on_delete=models.CASCADE)
-    info_product = models.ForeignKey(InfoProduct, verbose_name='Информация о продукте',related_name='ordered_items',
+                              blank=True, on_delete=models.CASCADE)
+    info_product = models.ForeignKey(InfoProduct, verbose_name='Информация о продукте', related_name='ordered_items',
                                      blank=True, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
     price = models.PositiveIntegerField(default=0, verbose_name='Цена')
@@ -131,12 +138,10 @@ class OrderItem(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['order_id', 'info_product'], name='unique_order_item'),
         ]
+
     def __str__(self):
         return f'№ {self.order} - {self.info_product.model}. Кол-во: {self.quantity}. Сумма: {self.total_cost}'
 
     def save(self, *args, **kwargs):
         self.total_cost = self.price * self.quantity
         super(OrderItem, self).save(*args, **kwargs)
-
-
-
