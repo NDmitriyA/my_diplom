46e6f97814cb80776625d9109791e901d44b3131
Dmitriy Nikiforov <nikiforov_d.1973@mail.ru>
2023-05-04 21:51:28 +0400

    shop_views_1

--- backendshop/shops/views.py
+++ backendshop/shops/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from backendshop.auth_user.models import ConfirmEmailToken
 from backendshop.shops.serializers import UserSerializer
 
 
@@ -9,7 +14,7 @@ class AccountRegister(APIView):
     """регистрация покупателей"""
     throttle_scope = 'anon'
 
-    def Post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         if {'first_name', 'last_name', 'email', 'password', 'company', 'position'}.issubset(request.data):
             errors = {}
             try:
@@ -29,5 +34,43 @@ class AccountRegister(APIView):
                     user.save()
                     return JsonResponse({'Status': True})
                 else:
-                    return JsonResponse
+                    return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны обходимые данные'})
+
+
+class AccountConfirm(APIView):
+    """подтверждение почтового адреса"""
+    throttle_scope = 'anon'
+
+    def post(self, request, *args, **kwargs):
+        if {'email', 'token'}.issubset(request.data):
+            token = ConfirmEmailToken.objects.fiter(user__email=request.data['email'],
+                                                    key=request.data['token']).first()
+            if token:
+                token.user.is_active = True
+                token.user.save()
+                token.delete()
+                return Response({'Status': True})
+            else:
+                return Response({'Status': False, 'Errors': 'Не правиль указан токен или email'})
+        return Response({'Status': False, 'Errors': 'Не указаны необходимые данные'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+class AccountLogin(APIView):
+    """авторизация пользователей"""
+
+    throttle_scope = 'anon'
+
+    def post(self, request, *args, **kwargs):
+        if {'email', 'password'}.issubset(request.data):
+            user = authenticate(request, username=request.data['email'], password=request.data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    token, _ = Token.objects.get_or_create(user=user)
+                    return Response({'Status': True, 'Token': token.key})
+            return Response({'Status': False, 'Errors': 'Ошибка авторизации'},
+                        status=status.HTTP_403_FORBIDDEN)
+        return Response({'Status': False, 'Errors': 'Не указаны необходимые данные'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
