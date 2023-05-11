74756ccc3f370f333cb5aadb7e8bd680d9c642db
Dmitriy Nikiforov <nikiforov_d.1973@mail.ru>
2023-05-11 20:22:13 +0400

    shop_views_2

--- backendshop/shops/views.py
+++ backendshop/shops/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
+from django.db.models import Q, Sum, F
 from django.http import JsonResponse
 from rest_framework import status, viewsets
 from rest_framework.authtoken.models import Token
@@ -7,8 +8,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from backendshop.auth_user.models import ConfirmEmailToken
-from backendshop.shops.models import Category, Shop
-from backendshop.shops.serializers import UserSerializer, CategorySerializer, ShopSerializer, ProductInfoSerializer
+from backendshop.shops.models import Category, Shop, InfoProduct, Order
+from backendshop.shops.serializers import UserSerializer, CategorySerializer, ShopSerializer, ProductInfoSerializer, \
+    OrderSerializer
 
 
 class AccountRegister(APIView):
@@ -57,6 +59,7 @@ class AccountConfirm(APIView):
         return Response({'Status': False, 'Errors': 'Не указаны необходимые данные'},
                         status=status.HTTP_400_BAD_REQUEST)
 
+
 class AccountLogin(APIView):
     """авторизация пользователей"""
 
@@ -71,7 +74,7 @@ class AccountLogin(APIView):
                     token, _ = Token.objects.get_or_create(user=user)
                     return Response({'Status': True, 'Token': token.key})
             return Response({'Status': False, 'Errors': 'Ошибка авторизации'},
-                        status=status.HTTP_403_FORBIDDEN)
+                            status=status.HTTP_403_FORBIDDEN)
         return Response({'Status': False, 'Errors': 'Не указаны необходимые данные'},
                         status=status.HTTP_400_BAD_REQUEST)
 
@@ -79,9 +82,10 @@ class AccountLogin(APIView):
 class DetailsAccount(APIView):
     """работа с данными пользователей"""
     throttle_scope = 'user'
+
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return Response({'Status': False, 'Error': 'Login required'},
+            return Response({'Status': False, 'Error': 'Требуется вход в систему'},
                             status=status.HTTP_403_FORBIDDEN)
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
@@ -89,8 +93,8 @@ class DetailsAccount(APIView):
     def post(self, request, *args, **kwargs):
         """создание данных пользователя"""
         if not request.user.is_authenticated:
-            return  Response({'Status': False, 'Error': 'Login required'},
-                             status=status.HTTP_403_FORBIDDEN)
+            return Response({'Status': False, 'Error': 'Требуется вход в систему'},
+                            status=status.HTTP_403_FORBIDDEN)
         """проверяем и сохраняем пароль"""
         if 'password' in request.data:
             try:
@@ -99,7 +103,7 @@ class DetailsAccount(APIView):
                 return Response({'Status': False, 'Errors': {'password': password_error}})
             else:
                 request.user.set_password(request.data['password'])
-        #проверяем остальные данные
+        # проверяем остальные данные
         user_serialiszer = UserSerializer(request.data, data=request.data, partial=True)
         if user_serialiszer.is_valid():
             user_serialiszer.save()
@@ -107,11 +111,14 @@ class DetailsAccount(APIView):
         else:
             return Response({'Status': False, 'Errors': user_serialiszer.errors},
                             status=status.HTTP_400_BAD_REQUEST)
+
+
 class CategoryView(viewsets.ModelViewSet):
     """просмотр категорий"""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     ordering = ('name',)
+
 
 class ShopView(viewsets.ModelViewSet):
     """просомтр списка магазинов"""
@@ -119,17 +126,46 @@ class ShopView(viewsets.ModelViewSet):
     serializer_class = ShopSerializer
     ordering = ('name',)
 
+
 class InfoProductView(viewsets.ReadOnlyModelViewSet):
     """поиск товаров"""
     throttle_scope = 'anon'
     serializer_class = ProductInfoSerializer
     ordering = ('product',)
 
+    def get_queryset(self):
+        query = Q(shop__state=True)
+        shop_id = self.request.query_params.get('shop_id')
+        category_id = self.request.query_params.get('category_id')
 
+        if shop_id:
+            query = query & Q(shop_id=shop_id)
 
+        if category_id:
+            query = query & Q(product__category_id=category_id)
 
+        # фильтруем и отбрасываем дубликаты
+        queryset = not InfoProduct.objects.filter(query).select_related(
+            'shop', 'product__category').prefetch_related(
+            'product_parameters__parameter').distinct()
+        return queryset
 
+class BasketView(APIView):
+    """работа с корзиной пользователя"""
 
+    throttle_scope = 'user'
 
+    #получить корзину
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status':False, 'Error': 'Требуется вход в систему'},
+                                status=status.HTTP_403_FORBIDDEN)
+        basket = Order.objects.filter(user_id=request.user.id, status='basket').prefetch_related(
+            'ordered_items__product_info__product_category',
+            'ordered_items__product_info__product_parameters__parameter').annotate(
+            total_sum=Sum(F('ordered_items__quantity') * F('ordered_items__product_info__price'))).distinct()
+
+        serializer = OrderSerializer(basket, many=True)
+        return Response(serializer.data)
 
 
