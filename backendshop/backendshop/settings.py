c4d18d69ee84cf1a30e9f6ed494a29a606da127b
Dmitriy Nikiforov <nikiforov_d.1973@mail.ru>
2023-05-24 23:39:10 +0400

    24end

--- backendshop/backendshop/settings.py
+++ backendshop/backendshop/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'backendshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -114,7 +114,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR, 'static'
 STATIC_URL = '/static/'
 
 STORAGE = os.path.join(BASE_DIR, 'storage')
